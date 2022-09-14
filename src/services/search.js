1facb37784e2cdf9aa7fea58f665d806ddde38fc
Belen <lenchu.deanta@gmail.com>
2022-09-14 12:33:48 +0200

    Movie IDs

--- src/services/search.js
+++ src/services/search.js
@@ -6,7 +6,7 @@ axios.defaults.baseURL = "https://swapi.dev/api/";
 export async function searchFilms(param) {
   try {
     const response = await axios.get("films/", { params: { search: param } });
-    return response.data.results;
+    return addMovieIds(response.data.results, "movie");
   } catch (e) {
     console.log("Error fetching films:", e);
     return [];
@@ -16,7 +16,7 @@ export async function searchFilms(param) {
 export async function searchPeople(param) {
   try {
     const response = await axios.get("people/", { params: { search: param } });
-    return response.data.results;
+    return addMovieIds(response.data.results);
   } catch (e) {
     console.log("Error fetching people:", e);
     return [];
@@ -26,9 +26,37 @@ export async function searchPeople(param) {
 export async function searchPlanets(param) {
   try {
     const response = await axios.get("planets/", { params: { search: param } });
-    return response.data.results;
+    return addMovieIds(response.data.results);
   } catch (e) {
     console.log("Error fetching people:", e);
     return [];
+  }
+}
+
+export async function getAllFilms() {
+  try {
+    const response = await axios.get("films/");
+    return addMovieIds(response.data.results, "movie");
+  } catch (e) {
+    console.log("Error fetching all films:", e);
+    return [];
+  }
+}
+
+function addMovieIds(arr, type) {
+  if (type === "movie") {
+    return arr.map((item) => ({
+      ...item,
+      movieId: item.url
+        .replace(`${axios.defaults.baseURL}films/`, "")
+        .replace("/", ""),
+    }));
+  } else {
+    return arr.map((item) => ({
+      ...item,
+      movieIds: item.films.map((url) =>
+        url.replace(`${axios.defaults.baseURL}films/`, "").replace("/", "")
+      ),
+    }));
   }
 }
