96cf067cc2865f926e4ffa37af460817c976b964
Belen <lenchu.deanta@gmail.com>
2022-09-14 09:53:50 +0200

    First commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">Search films</header>
+      <div><input type="text" />
+      <button onClick={()=>{}}>GO</button>
+      </div>
+    </div>
+  );
+}
+
+export default App;
